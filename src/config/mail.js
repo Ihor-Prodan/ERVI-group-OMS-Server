598df017aedd5936e424590a25ae9c3f4b79f235
Ihor Prodan <prodanigor0@gmail.com>
2025-11-03 22:17:59 +0100

    fix mail sendler

--- src/config/mail.js
+++ src/config/mail.js
@@ -1,28 +1,7 @@
-import nodemailer from "nodemailer";
+// import nodemailer from "nodemailer";
+import { Resend } from 'resend';
 
-const host = process.env.SMTP_HOST;
-const port = process.env.SMTP_PORT ? Number(process.env.SMTP_PORT) : undefined;
-const user = process.env.SMTP_USER;
-const pass = process.env.SMTP_PASS;
-const secure = process.env.SMTP_SECURE;
-
-export const transporter =
-  host && port && user && pass
-    ? nodemailer.createTransport({
-        host: host,
-        port: port,
-        secure: secure === "true",
-        auth: { user: user, pass: pass },
-      })
-    : null;
-
-if (transporter) {
-  console.log(
-    `SMTP configured: ${host}:${port} (secure=${secure === "true"})`
-  );
-} else {
-  console.warn("⚠️ SMTP not configured properly. Emails will not be sent.");
-}
+const resend = new Resend(process.env.RESEND_API_KEY);
 
 export const sendOrderEmail = async ({
   to,
@@ -30,13 +9,17 @@ export const sendOrderEmail = async ({
   text,
   attachments = [],
 }) => {
-  await transporter.sendMail({
-    from: `"${process.env.MAIL_SENDER_NAME || "Doručovacia služba"}" <${
-      process.env.SMTP_USER
-    }>`,
-    to,
-    subject,
-    text,
-    attachments,
-  });
+  try {
+    await resend.emails.send({
+      from: `${process.env.MAIL_SENDER_NAME || "Doručovacia služba"} <${process.env.SMTP_USER}>`,
+      to,
+      subject,
+      text,
+      attachments,
+    });
+
+    console.log(`✅ Email sent to ${to}`);
+  } catch (err) {
+    console.error('❌ Chyba pri odosielaní emailu cez Resend:', err);
+  }
 };
